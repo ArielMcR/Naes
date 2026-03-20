c854114bd8b023477716aaf929af7c6604741972
unknown <arielmachador3@gmail.com>
2026-03-20 20:56:25 -0300

    alterando models e adicionando context na view

--- reqManager/views.py
+++ reqManager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Projeto,Arquivo, Requisito, RegraDeNegocio, RequisitoDadosRNF, RequisitoDadosRF
 
 
 MOCK_PROJECTS = [
@@ -61,7 +62,9 @@ MOCK_PROJECTS = [
 
 
 class ListProjectView(TemplateView):
+    # model: Projeto
     template_name = "reqManager/Projetos/ListarProjetos.html"
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
